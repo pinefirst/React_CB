9b0bd0fa4f691fa487c07e93b530393e043c365f
hours_android <pinefirst@outlook.com>
2020-04-22 16:57:29 +0900

    add Homemenu in  header part

---
+++ front/src/components/LayoutComponents/Toolbar/HomeMenu/index.js
@@ -0,0 +1,70 @@
+import React from "react";
+import {Menu, Dropdown} from "antd";
+
+
+class HomeMenu extends React.Component{
+
+  render() {
+    const menu = (
+      <Menu selectable={false}>
+        <div className="topbar__activity">
+          <div className="topbar__activity__item">
+            <i className="topbar__activity__icon icmn-st  ar-full" />
+            <div className="topbar__activity__inner">
+              <div className="topbar__activity__title">
+                <span className="pull-right">now</span>
+                <a href="javascript: void(0);">
+                  Update Status: <span className="badge badge-danger">New</span>
+                </a>
+              </div>
+              <div className="topbar__activity__descr">
+                Failed to get available update data. To ensure the proper functioning of your
+                application, update now.
+              </div>
+            </div>
+          </div>
+          <div className="topbar__activity__item">
+            <i className="topbar__activity__icon icmn-stack" />
+            <div className="topbar__activity__inner">
+              <div className="topbar__activity__title">
+                <span className="pull-right">24 min ago</span>
+                <a href="javascript: void(0);">
+                  Income: <span className="badge badge-default">$299.00</span>
+                </a>
+              </div>
+              <div className="topbar__activity__descr">
+                Failed to get available update data. To ensure the proper functioning of your
+                application, update now.
+              </div>
+            </div>
+          </div>
+          <div className="topbar__activity__item">
+            <i className="topbar__activity__icon icmn-list" />
+            <div className="topbar__activity__inner">
+              <div className="topbar__activity__title">
+                <span className="pull-right">30 min ago</span>
+                <a href="javascript: void(0);">Inbox Message</a>
+              </div>
+              <div className="topbar__activity__descr">
+                From: <a href="javascript: void(0);">David Bowie</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </Menu>
+    )
+    return (
+      <div className="topbar__dropdown d-inline-block mr-4">
+        <Dropdown overlay={menu} trigger={['click']} placement="bottomRight">
+          <a className="ant-dropdown-link" href="/">
+            <i className="icmn-home topbar__dropdownIcon" />
+          </a>
+        </Dropdown>
+      </div>
+    )
+  }
+}
+
+
+export default HomeMenu;
--- front/src/components/LayoutComponents/Toolbar/index.js
+++ front/src/components/LayoutComponents/Toolbar/index.js
@@ -3,16 +3,19 @@ import {Button} from "antd";
 
 import './style.scss';
 import ProfileMenu from "./ProfileMenu";
+import HomeMenu from "./HomeMenu";
 
 class Toolbar extends React.Component{
 
   render() {
+
     return(
       <div className="topbar">
         <div className="topbar__left" >
 
         </div>
         <div className="topbar__right" >
+          <HomeMenu/>
           <ProfileMenu/>
         </div>
       </div>
